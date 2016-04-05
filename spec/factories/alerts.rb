1610da8f7cebe4bcffb2f0e6f467cf20c29e9b93
mafernando <fernando_michael@bah.com>
2016-04-05 16:19:57 -0500

    Fix dates where interpreted as grouped expression

--- spec/factories/alerts.rb
+++ spec/factories/alerts.rb
@@ -51,15 +51,15 @@ FactoryGirl.define do
       message 'This alert is within its start and end dates.'
       category 'foobar'
       start_date Time.zone.now.to_s # START DATE NOW
-      end_date (Time.zone.now + 1.day).to_s # END DATE NOT SET
+      end_date((Time.zone.now + 1.day).to_s) # END DATE NOT SET
     end
 
     trait :inactive do
       status 'warning'
       message 'This alert is not within its start and end dates.'
       category 'foobar'
-      start_date (Time.zone.now - 2.days).to_s # YESTERDAY - 1
-      end_date (Time.zone.now - 1.day).to_s # YESTERDAY
+      start_date((Time.zone.now - 2.days).to_s) # YESTERDAY - 1
+      end_date((Time.zone.now - 1.day).to_s) # YESTERDAY
     end
   end
 end
