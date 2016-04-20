ad3e79f23504fecd2a3915a6708f72d1e775ce03
Chris Kacerguis <kacerguis_christopher@bah.com>
2016-04-20 09:27:39 -0500

    added thing to get it to work.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,6 +24,8 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
+  config.serve_static_assets = ENV['RAILS_SERVE_STATIC_FILES'].present?
+
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = Uglifier.new(mangle: false)
   config.assets.css_compressor = :sass
